05d11e47137b7e82e29b49791b819c1e7b75827b
Maighteu <maight.progra@gmail.com>
2025-09-17 22:58:57 +0200

    first commit

---
+++ CMakeFiles/TerminalClient.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TerminalClient.dir/ClientConsultationBookerQt/main.cpp.o"
+  "CMakeFiles/TerminalClient.dir/ClientConsultationBookerQt/mainwindowclientconsultationbooker.cpp.o"
+  "CMakeFiles/TerminalClient.dir/ClientConsultationBookerQt/moc_mainwindowclientconsultationbooker.cpp.o"
+  "TerminalClient"
+  "TerminalClient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TerminalClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
