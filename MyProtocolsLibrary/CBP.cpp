5480d47d5ca1ef9fe0d0066ae62b42db78a7cf74
Maighteu <maight.progra@gmail.com>
2025-10-18 14:51:50 +0200

    try

--- MyProtocolsLibrary/CBP.cpp
+++ MyProtocolsLibrary/CBP.cpp
@@ -1,2 +1,96 @@
 #include "CBP.h"
 
+bool CBP(char* requete, char* reponse, const int socket)
+{
+	char *buffer = strtok(requete,"#");
+	//LOGIN
+	//////////////////////////////////////////////////////
+	if (strcmp(buffer,LOGIN) == 0)
+	{
+		if (CBP_LOGIN( buffer) >=0)
+		{
+			sprintf(reponse, "LOGIN#ok#");
+			return true;
+		}
+
+		else
+		{
+			sprintf(reponse, "LOGIN#ko#");
+			return false;
+		}
+	}
+}
+	
+ 
+
+int CBP_LOGIN(char* buffer)
+{
+
+
+		char prenom[20], nom[20];
+		int id;
+		buffer = strtok(NULL,"#");
+
+		if (strcmp(buffer,"NEW") == 0)
+		{
+			strcpy(nom, strtok(NULL,"#"));
+			strcpy(prenom, strtok(NULL,"#"));
+			if((id=AjouterPatient(nom,prenom)) > 0)
+			{	
+				return id;
+			}
+			else
+			{
+				return -2;
+			}
+		}
+		else if (strcmp(buffer,"OLD") == 0)
+		{
+			strcpy(nom, strtok(NULL,"#"));
+			strcpy(prenom, strtok(NULL,"#"));
+			id = stoi(strtok(NULL,"#"));
+
+			if (AuthentifierPatient(nom, prenom, id) >=0)
+			{
+				return id;
+			}
+			else
+			{
+				return -1;
+			}
+		}
+}
+
+bool CBP_LOGOUT(const int* id)
+{
+
+}
+
+std::list<specialite> CBP_GET_SPECIALTIES()
+{
+
+}
+
+std::list<docteur> CBP_GET_DOCTORS()
+{
+
+}
+
+std::list<consultation> CBP_SEARCH_CONSULTATIONS()
+{
+
+}
+
+bool CBP_BOOK_CONSULTATIONS(const int idConsultation, const int idPatient, const char* raison)
+{
+
+}
+
+void CBP_CLOSE()
+{
+
+}
+
+int  estPresent(int socket); 
+void ajoute(int socket); 
+void retire(int socket); 
--- MyProtocolsLibrary/CBP.h
+++ MyProtocolsLibrary/CBP.h
@@ -1,5 +1,47 @@
 #ifndef CBP_H
 #define CBP_H
+#include "sqlQuerry.h"
+using namespace std;
 
+// structure utile
+typedef struct{
+int id,
+char specialite[20],
+char doctor[40],
+char date[12],
+char hour [6]
+}consultation;
+
+typedef struct{
+int id,
+int nom[40]
+}docteur;
+
+typedef struct{
+	int id,
+	char nom
+}specialite;
+
+//'Machine à état'
+bool CBP(char* requete, char* reponse, const int socket);
+
+// Gére LOGIN nvx user ou non et verifie accés unique ([thread] [-] [socket]
+// -> [thread] [userId] [socket], si userId pas déja présent)
+bool CBP_LOGIN(char* buffer);
+
+//utilise l'ip pour retirer de la liste de connexion
+bool CBP_LOGOUT(const int* id);
+
+std::list<specialite> CBP_GET_SPECIALTIES();
+std::list<docteur> CBP_GET_DOCTORS();
+std::list<consultation> CBP_SEARCH_CONSULTATIONS();
+
+bool CBP_BOOK_CONSULTATIONS(const int idConsultation, const int idPatient, const char* raison);
+
+void CBP_CLOSE();
+
+int  estPresent(int socket); 
+void ajoute(int socket); 
+void retire(int socket); 
 
 #endif
